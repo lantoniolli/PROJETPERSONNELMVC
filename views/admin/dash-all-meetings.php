f6374ddbc060d40ff465a08300b6c5cb7f6e10a8
Antoniolli Laura <laura.antoniolli@outlook.fr>
2022-12-04 01:46:36 +0100

    meetings

--- views/admin/dash-all-meetings.php
+++ views/admin/dash-all-meetings.php
@@ -14,7 +14,7 @@
             <div class="col-4">
                 <form method="get">
                     <div class="input-group mb-4">
-                        <input type="search" class="form-control" placeholder="Chercher un article" name="search" aria-describedby="button-addon2">
+                        <input type="search" class="form-control" placeholder="Chercher une convention" name="search" aria-describedby="button-addon2">
                         <button class="btn btn__color" type="submit" id="button-addon2">Rechercher</button>
                     </div>
                 </form>
@@ -40,10 +40,12 @@
                     <td class="text-center"><?= $meeting->event_name ?></td>
                     <td class="text-truncate"><?= $meeting->event_description ?></td>
                     <td class="text-center">
-                        <!-- Bouton pour visualiser l'article de la convention -->
-                        <a href="/controllers/readnewsCtrl.php?id=<?= $meeting->id_meetings ?>"><i class="uil uil-eye button__icon__alt"></i></a>
-                        <!-- Bouton pour supprimer le commentaire -->
-                        <a href="/controllers/dashboard/dash-delete-commentsCtrl.php?id=<?= $meeting->id_meeting; ?>"><i class="uil uil-trash button__icon"></i></a>
+                        <!-- Bouton pour voir le post de la convention -->
+                        <a href="/controllers/"><i class="uil uil-eye button__icon__alt"></i></a>
+                        <!-- Bouton pour modifier la convention -->
+                        <a href="/controllers/dashboard/dash-modify-meetingCtrl.php?id=<?= $meeting->id_meetings ?>"><i class="uil uil-comment-alt-edit button__icon"></i></a>
+                        <!-- Bouton pour supprimer la convention -->
+                        <a href="/controllers/dashboard/dash-delete-newsCtrl.php?id=<?= $meeting->id_meetings ?>"><i class="uil uil-trash button__icon"></i></a>
                     </td>
                 </tr>
             <?php
